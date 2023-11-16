f58cc0dccdb94ff6558fa707e5d516b8690a9e6b
rianlesmana <rian.lesmana9999@gmail.com>
2023-11-16 10:14:25 +0700

    set PWA

--- vite.config.js
+++ vite.config.js
@@ -8,25 +8,25 @@ export default defineConfig({
 });
 
 const manifestForPlugIn = {
-    registerType: "prompt",
-    includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
-    manifest: {
-        name: "React-vite-app",
-        short_name: "react-vite-app",
-        description: "I am a simple vite app",
-        icons: [
-            {
-                src: "./src/assets/img/icon-macaca.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "favicon",
-            },
-        ],
-        theme_color: "#171717",
-        background_color: "#f0e7db",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        orientation: "portrait",
-    },
+    registerType: "autoUpdate",
+    // includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+    // manifest: {
+    //     name: "React-vite-app",
+    //     short_name: "react-vite-app",
+    //     description: "I am a simple vite app",
+    //     icons: [
+    //         {
+    //             src: "./src/assets/img/icon-macaca.png",
+    //             sizes: "512x512",
+    //             type: "image/png",
+    //             purpose: "favicon",
+    //         },
+    //     ],
+    //     theme_color: "#171717",
+    //     background_color: "#f0e7db",
+    //     display: "standalone",
+    //     scope: "/",
+    //     start_url: "/",
+    //     orientation: "portrait",
+    // },
 };
