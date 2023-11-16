32d023b5cefdc8e97c152d280f3d33d0753605fa
rianlesmana <rian.lesmana9999@gmail.com>
2023-11-16 10:42:12 +0700

    set PWA

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: "autoUpdate",
-            includeAssets: ["./src/assets/img/icon-macaca.png"],
+            includeAssets: ["icon-macaca.png"],
             manifest: {
                 name: "Mangrove Carbon Calculator",
                 short_name: "Macaca",
