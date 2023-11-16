d100ce2bea77b0cd80ff1598377d2bc53b4f80b0
rianlesmana <rian.lesmana9999@gmail.com>
2023-11-16 10:29:06 +0700

    set PWA

--- vite.config.js
+++ vite.config.js
@@ -4,29 +4,34 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA(manifestForPlugIn)],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+        }),
+    ],
 });
 
-const manifestForPlugIn = {
-    registerType: "autoUpdate",
-    includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
-    manifest: {
-        name: "Mangrove Carbon Calculator",
-        short_name: "Macaca",
-        description: "Macaca Apps",
-        icons: [
-            {
-                src: "./src/assets/img/icon-macaca.png",
-                sizes: "144px",
-                type: "image/png",
-                purpose: "favicon",
-            },
-        ],
-        theme_color: "#171717",
-        background_color: "#f0e7db",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        orientation: "portrait",
-    },
-};
+// const manifestForPlugIn = {
+//     registerType: "autoUpdate",
+//     includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+//     manifest: {
+//         name: "Mangrove Carbon Calculator",
+//         short_name: "Macaca",
+//         description: "Macaca Apps",
+//         icons: [
+//             {
+//                 src: "./src/assets/img/icon-macaca.png",
+//                 sizes: "144px",
+//                 type: "image/png",
+//                 purpose: "favicon",
+//             },
+//         ],
+//         theme_color: "#171717",
+//         background_color: "#f0e7db",
+//         display: "standalone",
+//         scope: "/",
+//         start_url: "/",
+//         orientation: "portrait",
+//     },
+// };
